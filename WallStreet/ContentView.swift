7c1df8b0c85c679974c2343cff0f82e6ee96353d
vansh sharma <vanshsharma5503@gmail.com>
2025-03-24 01:13:52 +0530

    Initial Commit

---
+++ WallStreet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WallStreet
+//
+//  Created by Vansh Sharma on 24/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WallStreet/WallStreetApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallStreetApp.swift
+//  WallStreet
+//
+//  Created by Vansh Sharma on 24/03/25.
+//
+
+import SwiftUI
+
+@main
+struct WallStreetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
